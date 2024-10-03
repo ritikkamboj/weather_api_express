1d024064590107b281b123ba74b682541757a110
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-03 18:38:36 +0530

    facing error even using axios

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config,env" });
+const fetch = require('node-fetch');
+
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+
 
 const apikey = process.env.API_KEY;
 const port = process.env.PORT;
 
 
-const app = express();
 
 app.get("/", function (req, res) {
-    console.log(req.query.address);
-    const address = req.query.address;
 
-    // res.send("jai maata di");
-    let url = `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${address}`;
-    axios.get(url).then(res => {
-        console.log(res.location.country);
+    const q = req.query.q;
+
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${q}`;
+
+    axios.get(url).then(response => {
+
+        const data = response.data;
+        // console.log(data);
+
 
     })
 
-
+    res.send('jai shree ram ')
 });
 
 app.listen(3000, function () {
     console.log("server is running");
+    console.log(port);
+
 });
 
 // http://api.weatherapi.com/v1/current.json?key=e59217de1a6c48dda54124038240210&q=ladwa
